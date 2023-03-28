6d8a7014dda55fe14880dc386e3caf2d073bca31
Leonardo Zwierzykowski <leonardozw46@gmail.com>
2023-03-28 13:59:39 -0300

    Aula 27/03 - Integração com banco H2 e primeira entidade

---
+++ demo/src/main/java/com/pettopia/demo/entity/Cliente.java
@@ -0,0 +1,41 @@
+package com.pettopia.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 600)
+    private String nome;
+    @Temporal(value = TemporalType.DATE)
+    private Date dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
