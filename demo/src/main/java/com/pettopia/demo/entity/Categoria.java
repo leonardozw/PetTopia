ed36465f3c00c858b3edc7de7d7af507163b06b9
leonardo <leonardozw46@gmail.com>
2023-05-12 20:15:36 -0300

    Conexão Produto e Categoria

--- demo/src/main/java/com/pettopia/demo/entity/Categoria.java
+++ demo/src/main/java/com/pettopia/demo/entity/Categoria.java
@@ -1,9 +1,13 @@
 package com.pettopia.demo.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Categoria {
@@ -11,6 +15,10 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Produto> produtos =  new HashSet<>();
+
     
     public Categoria() {
     }
