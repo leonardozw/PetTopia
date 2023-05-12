2c89123dc4acc5d360dc05abf63646f2b98c4cb6
leonardo <leonardozw46@gmail.com>
2023-05-12 19:47:39 -0300

    Design

--- demo/src/main/java/com/pettopia/demo/controller/HomeController.java
+++ demo/src/main/java/com/pettopia/demo/controller/HomeController.java
@@ -13,9 +13,6 @@ public class HomeController {
 
     @GetMapping
     public ModelAndView index(){
-        HashMap<String, String> map = new HashMap<>();
-        map.put("apelido","valor");
-        map.put("nome","zezinho");
-        return new ModelAndView("home/index",map);
+        return new ModelAndView("home/index");
     }
 }
