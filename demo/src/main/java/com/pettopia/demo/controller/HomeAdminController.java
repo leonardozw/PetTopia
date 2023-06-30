4a824bacc2ff0193c65c2ad4adaa706f4b3d99b5
Marcos Godinho <marcos.godinho@univille.br>
2023-06-30 20:16:57 -0300

    Tela de Dashboard

--- demo/src/main/java/com/pettopia/demo/controller/HomeAdminController.java
+++ demo/src/main/java/com/pettopia/demo/controller/HomeAdminController.java
@@ -1,17 +1,25 @@
 package com.pettopia.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pettopia.demo.service.ClienteService;
+
 @Controller
 @RequestMapping("/homeAdmin")
 public class HomeAdminController {
 
+    @Autowired
+    private ClienteService clienteService;
+
     @GetMapping
     public ModelAndView index(){
-        return new ModelAndView("homeAdmin/index");
+        var listaClientes = clienteService.getAll();
+        return new ModelAndView("homeAdmin/index",
+                    "listaClientes",listaClientes);
     }
 
 }
