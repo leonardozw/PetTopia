696de21ba40d3c0267b13d92e826f12287ef98e0
Leonardo Zwierzykowski <leonardozw46@gmail.com>
2023-07-11 17:35:44 -0300

    fixes

--- demo/src/main/java/com/pettopia/demo/repository/CategoriaRepository.java
+++ demo/src/main/java/com/pettopia/demo/repository/CategoriaRepository.java
@@ -3,11 +3,9 @@ package com.pettopia.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pettopia.demo.entity.Categoria;
 
-@Repository
 public interface CategoriaRepository 
     extends JpaRepository<Categoria, Long>{
         List<Categoria> findByNomeContainingIgnoreCase(String nome);
