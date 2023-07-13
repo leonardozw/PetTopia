400610b70fcd3f41eb7b28dd470e8737ac0cc3f9
MarcosGodinho <marcos.godinho@univille.br>
2023-07-13 19:05:56 -0300

    Login Azure

--- demo/src/main/java/com/pettopia/demo/controller/HomeAdminController.java
+++ demo/src/main/java/com/pettopia/demo/controller/HomeAdminController.java
@@ -1,6 +1,7 @@
 package com.pettopia.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import com.pettopia.demo.service.ClienteService;
 
 @Controller
 @RequestMapping("/homeAdmin")
+@PreAuthorize("hasAuthority('APPROLE_admin')")
 public class HomeAdminController {
 
     @Autowired
